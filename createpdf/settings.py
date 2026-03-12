d46c1b380d0fedacd3340433a16055df944dba8c
Johnson Obajuluwa <155059646+ayoobas@users.noreply.github.com>
2026-03-12 22:35:37 +0000

    Remove SMTP email configuration from settings
    Removed email configuration settings for SMTP backend.

--- createpdf/settings.py
+++ createpdf/settings.py
@@ -127,12 +127,7 @@ MEDIA_ROOT = (BASE_DIR / 'media')
 MEDIA_URL = '/media/'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tureal162@gmail.com'
-EMAIL_HOST_PASSWORD = 'gqezehsnmxfvzyme' # Use a Google App Password, not your real password
+
 
 
 
